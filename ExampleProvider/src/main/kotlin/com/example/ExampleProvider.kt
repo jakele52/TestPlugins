1ca1929699bc0be56b0be7d7e30dcf1654d61282
jakele52 <122524005+jakele52@users.noreply.github.com>
2026-05-20 16:18:47 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,13 +1,13 @@
-package com.example
-
-import com.lagradost.cloudstream3.*
-
-class ExampleProvider : MainAPI() {
-    override var mainUrl = "https://example.com"
-    override var name = "Example"
-    override val supportedTypes = setOf(TvType.Movie)
-
-    override suspend fun search(query: String): List<SearchResponse> {
-        return emptyList()
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PRESERVE_EXISTING)
+}
+
+rootProject.name = "TestPlugins"
+include(":ExampleProvider")
