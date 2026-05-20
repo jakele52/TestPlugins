2778655a71660990566564fda252cd8ca2468f52
jakele52 <122524005+jakele52@users.noreply.github.com>
2026-05-20 16:22:59 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 buildscript {
     repositories {
         google()
@@ -23,51 +19,6 @@ allprojects {
     }
 }
 
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    configure {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
-    }
-
-    configure {
-        namespace = "com.example"
-        compileSdkVersion(35)
-
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
-
-    tasks.withType().configureEach {
-        kotlinOptions {
-            jvmTarget = "1.8"
-            freeCompilerArgs = freeCompilerArgs + listOf(
-                "-Xno-call-assertions",
-                "-Xno-param-assertions",
-                "-Xno-receiver-assertions",
-                "-Xskip-metadata-version-check"
-            )
-        }
-    }
-
-    dependencies {
-        "cloudstream"("com.lagradost:cloudstream3:pre-release")
-        "implementation"(kotlin("stdlib"))
-        "implementation"("com.github.Blatzar:NiceHttp:0.4.11")
-        "implementation"("org.jsoup:jsoup:1.18.3")
-        "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-    }
-}
-
-task("clean") {
+task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
