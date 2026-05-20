0fb61a692b4df1f3572be654b425b0ac64ceb0c8
jakele52 <122524005+jakele52@users.noreply.github.com>
2026-05-20 16:18:21 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -9,11 +8,10 @@ buildscript {
         mavenCentral()
         maven("https://jitpack.io")
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
@@ -25,33 +23,21 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
-
 subprojects {
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("2.1.0")
-            }
-        }
-    }
-
-    cloudstream {
+    configure {
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
     }
 
-    android {
+    configure {
         namespace = "com.example"
+        compileSdkVersion(35)
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
             targetSdk = 35
         }
 
@@ -59,34 +45,29 @@ subprojects {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
+    }
 
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
-                // SỬA TẠI ĐÂY: Dùng .set(listOf(...)) để truyền danh sách tham số đúng cú pháp Gradle Kotlin DSL
-                freeCompilerArgs.set(listOf(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xskip-metadata-version-check"
-                ))
-            }
+    tasks.withType().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-Xno-call-assertions",
+                "-Xno-param-assertions",
+                "-Xno-receiver-assertions",
+                "-Xskip-metadata-version-check"
+            )
         }
     }
 
     dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
-
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        "cloudstream"("com.lagradost:cloudstream3:pre-release")
+        "implementation"(kotlin("stdlib"))
+        "implementation"("com.github.Blatzar:NiceHttp:0.4.11")
+        "implementation"("org.jsoup:jsoup:1.18.3")
+        "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     }
 }
 
-task<Delete>("clean") {
+task("clean") {
     delete(rootProject.layout.buildDirectory)
 }
